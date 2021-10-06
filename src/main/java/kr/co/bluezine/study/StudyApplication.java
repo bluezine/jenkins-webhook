77e8d23aff3551abe343b350323a2bbaeeffbb65
ksseo-i2max <ksseo@i2max.co.kr>
2021-10-06 16:47:50 +0900

    WebHook Test

--- src/main/java/kr/co/bluezine/study/StudyApplication.java
+++ src/main/java/kr/co/bluezine/study/StudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello WebHook Test");
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
